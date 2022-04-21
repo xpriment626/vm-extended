ac8a20b2752138a3dc2ef3dd4ba2fc6d339e2424
Emmett Childs <emmettchilds@thegobospot.com>
2022-04-21 10:41:55 +0800

    import statements

--- src/VmExtended.sol
+++ src/VmExtended.sol
@@ -1,9 +1,19 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.10;
 
-import { Vm } from "./Vm.sol";
-contract VmExtended {
+import { Vm } from "./interfaces/Vm.sol";
+import { DSTest } from "../lib/ds-test/src/test.sol";
+import { IERC20 } from "./interfaces/IERC20.sol";
+contract VmExtended is DSTest {
 
     // Initialises vm_extended with HEVM address
-    Vm public constant vm_extended = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
+    Vm public constant vm_extended = Vm(HEVM_ADDRESS);
+
+    function initWithETH(uint256 _id) public returns (address) {
+        address user = vm_extended.addr(_id);
+        vm_extended.deal(user, 100 ether);
+
+        return user;
+    }
+
 }
