fac1414a7adc021e65a101053d47b4ee6e658624
Emmett Childs <emmettchilds@thegobospot.com>
2022-05-01 10:20:35 +0800

    added kleros constants

--- src/Constants.sol
+++ src/Constants.sol
@@ -16,6 +16,10 @@ abstract contract Constants {
     address public constant UNI_V3_ROUTER = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
     address public constant UNI_V3_FACTORY = 0xE592427A0AEce92De3Edee1F18E0157C05861564;
 
+    /// Kleros constants
+    address public constant KLEROS_LIQ = 0x988b3A538b618C7A603e1c11Ab82Cd16dbE28069;
+    address public constant KLEROS_PNK = 0x93ED3FBe21207Ec2E8f2d3c3de6e058Cb73Bc04d;
+
     /// Core Pricefeed IDs
     uint256 public constant ETH_USD = 1;
     uint256 public constant BTC_USD = 2;
@@ -37,5 +41,4 @@ abstract contract Constants {
     uint256 public constant INCH_USD = 14;
     uint256 public constant YFI_USD = 15;
     uint256 public constant KNC_USD = 16;
-
 }
